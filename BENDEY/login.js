b50f8de0f9c2a2ed3a9ea5cb79145affae6a6881
Juniorrdev18 <140906701+Juniorrdev18@users.noreply.github.com>
2023-09-01 17:05:27 -0500

    Add files via upload

---
+++ BENDEY/login.js
@@ -0,0 +1,14 @@
+function loguear ()
+{
+    let user=document.getElementById("usuario").value; 
+    let pass=document.getElementById("clave").value;
+
+
+    if(user=="Juan" && pass=="1234") {
+        window.location="inicio.html"
+    }
+
+    else {
+        alert("Datos incorrectos")
+    }
+}
